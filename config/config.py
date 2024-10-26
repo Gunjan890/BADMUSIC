174e9a9021f2c4f6d353efefe07479a67e7c96d3
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-10-27 00:01:15 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -45,7 +45,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/Gunjan890/Badmusic",
+    "https://github.com/Badhacker98/BADMUSIC_PLUGIN",
 )
 # Fill here the external plugins repo where plugins that you want to load
 

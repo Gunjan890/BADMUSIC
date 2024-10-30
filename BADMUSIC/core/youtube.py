e28a662584726b4e7e11349dd8fc3dda0c558654
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-10-30 17:05:49 +0530

    Update youtube.py

--- BADMUSIC/core/youtube.py
+++ BADMUSIC/core/youtube.py
@@ -2,14 +2,13 @@ import json
 import os
 
 YOUTUBE = {
-    "access_token": "ya29.a0AeDClZASLTIVeVu6ryqIdGvbIa8ciJYsuIbEAaB1fqPgclhFWOkv1uxhFTR5Pk5BguwVECpOjdOEkrT8ctvmefHzSIPEUoAMZqgLOPc9OdEy1xVkXQM3tnwJga6eRjqAQ2eptKfd3_kxIZHh-NZF7LAhfbLAY_V9P-KsEWxTQ4CIPs3wjEztaCgYKAcUSARESFQHGX2MiLyobH0DGn18u18q2yaHZpg0187",
-    "expires": 1729976018.907575,
-    "refresh_token": "1//05AEnUA8gnn6nCgYIARAAGAUSNwF-L9Ir3759d17CZH48CJpeAd7Aw-BSYUz6LcbKvKXZJ3F8cmgTX3QvkZ3Uf4pR7-nCDeRNWPg",
+    "access_token": "ya29.a0AeDClZBIACc7kPPRpr6F1QS2W23FewZ7BUl4_RhJMNyuQPoDxAW4ZUmy1Joj71bg2hhCoOSy-8M-P9Dr3H71Iyd3Y3zV4muwvYClMlD4Yjbi_PBAIMajmPYt7Fd-3OUqTa10LkrjmSp8x8UtAy_Si84DxMHe_UQ4uBgNJgm487MXnyqbna_6aCgYKAbwSARASFQHGX2MifsiT-wpTFdqc1EBlhUHAFQ0187",
+    "expires": 1730267664.803879,
     "token_type": "Bearer",
+    "refresh_token": "1//05e_6MmGv8p13CgYIARAAGAUSNwF-L9IrQR-txkBUpPem86mSv2-5sLMahSswgSNeBa-pBdFlL_spq34l5WGD7Zoc3EbM6tatFVs"
 }
 
-
-def BADMUNDA():
+def badmunda():
     TOKEN_DATA = os.getenv("TOKEN_DATA")
     if not TOKEN_DATA:
         os.environ["TOKEN_DATA"] = json.dumps(YOUTUBE)
